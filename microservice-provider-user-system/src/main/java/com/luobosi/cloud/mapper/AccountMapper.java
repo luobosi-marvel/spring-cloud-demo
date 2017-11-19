43cf22f53b49c36debbf6507e110235e984dd622
luobosi <luobosi@2dfire.com>
2017-11-19 16:55:18 +0800

    完善 user-system 系统结构

---
+++ microservice-provider-user-system/src/main/java/com/luobosi/cloud/mapper/AccountMapper.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.luobosi.cloud.mapper;
+
+/**
+ * AccountMapper
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-11-19
+ */
+public interface AccountMapper {
+}
